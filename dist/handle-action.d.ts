6a81244507d7702695fa5178c0cd44397508b5b9
Ian Richardson <iantrich@gmail.com>
2021-06-12 09:34:10 -0500

    1.7.1

--- dist/handle-action.d.ts
+++ dist/handle-action.d.ts
@@ -5,7 +5,7 @@ export declare const handleActionConfig: (node: HTMLElement, hass: HomeAssistant
     hold_action?: ActionConfig;
     tap_action?: ActionConfig;
     double_tap_action?: ActionConfig;
-}, actionConfig: ActionConfig) => void;
+}, actionConfig: ActionConfig | undefined) => void;
 export declare const handleAction: (node: HTMLElement, hass: HomeAssistant, config: {
     entity?: string;
     camera_image?: string;
