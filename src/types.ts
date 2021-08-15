5d540927353ba57c6d1d81cda1afd8097491b0ef
JonahKr <jonah.kraushaar@gmx.net>
2021-08-15 14:05:16 +0200

    Updated new frontend types

--- src/types.ts
+++ src/types.ts
@@ -236,6 +236,28 @@ export interface HomeAssistant {
   callWS: <T>(msg: MessageBase) => Promise<T>;
 }
 
+export enum NumberFormat {
+  language = "language",
+  system = "system",
+  comma_decimal = "comma_decimal",
+  decimal_comma = "decimal_comma",
+  space_comma = "space_comma",
+  none = "none",
+}
+
+export enum TimeFormat {
+  language = "language",
+  system = "system",
+  am_pm = "12",
+  twenty_four = "24",
+}
+
+export interface FrontendLocaleData {
+  language: string;
+  number_format: NumberFormat;
+  time_format: TimeFormat;
+}
+
 export interface LovelaceCardConfig {
   index?: number;
   view_index?: number;
