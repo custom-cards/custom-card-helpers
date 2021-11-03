f029e8a80d46980db76a72f15594fff62a7de629
JonahKr <jonah.kraushaar@gmx.net>
2021-11-03 19:09:56 +0100

    Updating Date Formatting functions

--- src/datetime/format_date_time.ts
+++ src/datetime/format_date_time.ts
@@ -1,23 +1,70 @@
-import fecha from "fecha";
-import { FrontendTranslationData } from "../types";
+//REF: https://github.com/home-assistant/frontend/blob/dev/src/common/datetime/format_date_time.ts
 
-// Check for support of native locale string options
-function toLocaleStringSupportsOptions() {
-  try {
-    new Date().toLocaleString("i");
-  } catch (err) {
-    return (err as Error).name === "RangeError";
-  }
-  return false;
-}
+import { FrontendLocaleData } from "../types";
+import { useAmPm } from "./use_am_pm";
 
-export const formatDateTime = (toLocaleStringSupportsOptions()
-  ? (dateObj: Date, locales: FrontendTranslationData) =>
-      dateObj.toLocaleString(locales.language, {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "2-digit",
-      })
-  : (dateObj: Date) => fecha.format(dateObj, "haDateTime"));
+// August 9, 2021, 8:23 AM
+/**
+ * Formatting a dateObject to date with time e.g. August 9, 2021, 8:23 AM
+ * @param dateObj The date to convert
+ * @param locale The users's locale settings
+ * @returns month and day like "August 9, 2021, 8:23 AM"
+ */
+export const formatDateTime = (dateObj: Date, locale: FrontendLocaleData) =>
+  formatDateTimeMem(locale).format(dateObj);
+
+const formatDateTimeMem = (locale: FrontendLocaleData) =>
+  new Intl.DateTimeFormat(locale.language, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: useAmPm(locale) ? "numeric" : "2-digit",
+    minute: "2-digit",
+    hour12: useAmPm(locale),
+  });
+
+
+/**
+ * Formatting a dateObject to date with time e.g. August 9, 2021, 8:23:15 AM
+ * @param dateObj The date to convert
+ * @param locale The users's locale settings
+ * @returns month and day like "August 9, 2021, 8:23:15 AM"
+ */
+export const formatDateTimeWithSeconds = (
+  dateObj: Date,
+  locale: FrontendLocaleData
+) => formatDateTimeWithSecondsMem(locale).format(dateObj);
+
+const formatDateTimeWithSecondsMem =
+  (locale: FrontendLocaleData) =>
+    new Intl.DateTimeFormat(locale.language, {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: useAmPm(locale) ? "numeric" : "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: useAmPm(locale),
+    });
+
+/**
+ * Formatting a Date to just date with AM/PM time e.g. 9/8/2021, 8:23 AM
+ * @param dateObj The date to convert
+ * @param locale The users's locale settings
+ * @returns month and day like "9/8/2021, 8:23 AM"
+ */
+export const formatDateTimeNumeric = (
+  dateObj: Date,
+  locale: FrontendLocaleData
+) => formatDateTimeNumericMem(locale).format(dateObj);
+
+const formatDateTimeNumericMem =
+  (locale: FrontendLocaleData) =>
+    new Intl.DateTimeFormat(locale.language, {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: useAmPm(locale),
+    });
